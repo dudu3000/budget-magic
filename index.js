141b63347337891d5e0e8be50d330b212f2aee76
Ionut-Alexandru Duduman <dudumanionut96@gmail.com>
2024-07-09 10:57:02 +0300

    feat: adding .env file and variables and also docker .env file and tested

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
+require('dotenv').config();
 const express = require('express');
-const budgetController = require('./src/controller/BudgetController');
-
-const app  = express();
 const bodyParser = require('body-parser');
+
+const budgetController = require('./src/controller/BudgetController');
 const db = require('./src/model/index');
 
+const app  = express();
 const { sequelize } = db;
-
 app.set('view engine', 'pug');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(budgetController);
 
 sequelize.authenticate().then(() => {
-    app.listen(3000, () => {
-        console.log('Listening on 3000');
+    app.listen(process.env.API_PORT, () => {
+        console.log(`Listening on ${process.env.API_PORT}`);
     });
 }).catch(error => {
     console.log('Unable to connect to db', error);
