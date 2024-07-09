585db874f3412057aaa961677dcc90f501c1cb0d
Ionut-Alexandru Duduman <dudumanionut96@gmail.com>
2024-07-09 12:07:47 +0300

    feat: adding the 3 new columns in model and in view

--- src/controller/BudgetController.js
+++ src/controller/BudgetController.js
@@ -6,6 +6,10 @@ var message = 'Bine ai venit pe pagina de buget';
 
 router.get('/', async (req, res) => {
     const records = await Budget.findAll();
+    records.map((record) => {
+        record.is_mandatory = record.is_mandatory ? 'Da' : 'Nu';
+        record.is_investment = record.is_investment ? 'Da' : 'Nu';
+    })
 
     res.render('home', {
         title: 'BudgetMagic',
@@ -18,12 +22,21 @@ router.post('/submit', async (req, res) => {
     const option = req.body.dropdown;
     const ammount = req.body.number;
     const name = req.body.name;
+    const isMandatory = req.body.isMandatory === 'Da';
+    const isInvestment = req.body.isInvestment === 'Da';
+    const investmentReturn = req.body.investmentReturn;
     message = `${name} a adaugat cu succes ${ammount} la categoria ${option}`;
 
+    console.log(isMandatory)
+    console.log(isInvestment)
+    console.log(req.body)
     await Budget.create({
         type: option,
         ammount: ammount,
-        name: name
+        name: name,
+        is_mandatory: isMandatory,
+        is_investment: isInvestment,
+        investment_return_percentage: investmentReturn
     });
 
     res.redirect('/');
